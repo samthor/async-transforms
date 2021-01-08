0e11d4e6efbb7a1ac9c29fae5de1b42225a5b4b9
Sam Thorogood <sam.thorogood@gmail.com>
2021-01-08 16:36:00 +1100

    make output cleaner

--- build.sh
+++ build.sh
@@ -6,11 +6,9 @@ cd src
 
 for SRC in **/*.js *.js; do
   echo $SRC
-  ../node_modules/.bin/rollup --format=cjs --file=../${SRC%.*}.cjs -- $SRC
+  ../node_modules/.bin/rollup --format=cjs --file=../require/${SRC%.*}.cjs -- $SRC
 done
 
-touch ../worker/index.js
+# We place the types here because TypeScript doesn't know about named subdir exports.
+mkdir -p ../worker
 cp worker/index.d.ts ../worker/index.d.ts
-
-touch ../index.js
-cp index.d.ts ../index.d.ts
