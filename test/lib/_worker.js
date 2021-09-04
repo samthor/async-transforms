a67ebfd3fd552e012c4bd03661470ac07be25385
Sam Thorogood <sam.thorogood@gmail.com>
2021-09-04 14:48:30 +1000

    wider range on test values (1ms too close)

--- test/lib/_worker.js
+++ test/lib/_worker.js
@@ -8,8 +8,8 @@ if (worker.isMainThread) {
  * @param {number} x
  */
 export default async function(x) {
-  // try to come back in reverse order
-  const delay = 40 - x;
+  // try to come back in reverse order (x is 5-10)
+  const delay = 50 - x * 5;
   await new Promise((r) => setTimeout(r, delay));
   return x + 10;
 }
